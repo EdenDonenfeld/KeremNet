53f79de670ae16c990114567aaec248f2c4e2276
EdenDonenfeld <edendonenfeld@gmail.com>
2025-06-23 16:57:30 +0300

    fixing home component

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css';
 import Post, { PostProps } from '../post/Post';
 
@@ -6,8 +6,30 @@ interface Props {
     postsFromJson: PostProps[];
 }
 
-const Home: React.FC<Props> = ({ postsFromJson }) => {
-    const [posts, setPosts] = useState<PostProps[]>(postsFromJson);
+const Home: React.FC<Props> = () => {
+    const [posts, setPosts] = useState<PostProps[]>();
+
+    const fetchAllPosts = async () => {
+        try {
+            const response = await fetch('http://localhost:3000/posts');
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            setPosts(data.posts);
+        } catch (error) {
+            console.error('Failed to fetch posts:', error);
+        }
+    }
+
+    useEffect(() => {
+        try {
+            fetchAllPosts();
+        } catch (error) {
+            console.error('Error fetching posts:', error);
+        }
+    }, []);
+
     return (
         <div className="home-container">
             <h1 className="title">KeremNet</h1>
