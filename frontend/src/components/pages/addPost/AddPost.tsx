e12dc9838e459068092920ba3c55dc3b6b0208c4
EdenDonenfeld <edendonenfeld@gmail.com>
2025-06-25 17:11:52 +0300

    add post component design

--- frontend/src/components/pages/addPost/AddPost.tsx
+++ frontend/src/components/pages/addPost/AddPost.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, TextField, Button } from '@mui/material';
 
 const AddPost = () => {
   return (
-    <Box>
-        <Typography variant="h4" gutterBottom>
+    <Box sx={{ maxWidth: 600, margin: 'auto', textAlign: 'center' }}>
+        <Typography variant="h5" gutterBottom>
             Add a New Post
         </Typography>
+        <form>
+            <TextField
+            fullWidth
+            label="Username"
+            variant="standard"
+            margin="normal"
+            />
+            <TextField
+            fullWidth
+            label="Post Content"
+            variant="outlined"
+            multiline
+            rows={4}
+            margin="normal"
+            />
+            <Button sx={{ marginTop: 2 }}variant="contained" color="secondary" type="submit">
+            Submit Post
+            </Button>
+        </form>
     </Box>
   );
 }
