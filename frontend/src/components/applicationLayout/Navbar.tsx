5c32360fb20435ef52928669173cae705a3f8f26
EdenDonenfeld <edendonenfeld@gmail.com>
2025-06-25 16:46:31 +0300

    started add post component

--- frontend/src/components/applicationLayout/Navbar.tsx
+++ frontend/src/components/applicationLayout/Navbar.tsx
@@ -6,6 +6,7 @@ const Navbar = () => {
     <Box sx={{ backgroundColor: '#ECC9EE', px: 2, py: 1 }}>
       <Stack direction="row" spacing={4} justifyContent="center">
         <Button color="secondary" variant="text" component={Link} to="/">Home</Button>
+        <Button color="secondary" variant="text" component={Link} to="/add-post">Add Post</Button>
       </Stack>
     </Box>
   );
---
+++ frontend/src/components/pages/addPost/AddPost.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+const AddPost = () => {
+  return (
+    <Box>
+        <Typography variant="h4" gutterBottom>
+            Add a New Post
+        </Typography>
+    </Box>
+  );
+}
+
+export default AddPost;
