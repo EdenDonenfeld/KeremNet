8d0c3fb70c1ebf717657a7dfada77668fcdbfa41
EdenDonenfeld <edendonenfeld@gmail.com>
2025-06-26 15:27:38 +0300

    fixed session user

--- backend/controllers/authController.ts
+++ backend/controllers/authController.ts
@@ -15,6 +15,7 @@ export const registerUser = async (req: Request, res: Response): Promise<void> =
 };
 
 export const loginUser = async (req: Request, res: Response): Promise<void> => {
+  console.log("Hi im here");
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     res.status(400).json({ errors: errors.array() });
@@ -24,7 +25,9 @@ export const loginUser = async (req: Request, res: Response): Promise<void> => {
   const result = await login(username, password);
 
   if (result.status === 200) {
-    req.session.user = result.data.username;
+    console.log("User logged in successfully2");
+    req.session.user = result.data.user.username;
+    console.log("Session user:", result.data);
   }
 
   res.status(result.status).json(result.data);
