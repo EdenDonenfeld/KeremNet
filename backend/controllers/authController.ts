c96774e1c03e4383391d5fb3b708e2465ddfde6a
EdenDonenfeld <edendonenfeld@gmail.com>
2025-06-26 17:09:41 +0300

    small bug fix

--- backend/controllers/authController.ts
+++ backend/controllers/authController.ts
@@ -24,7 +24,7 @@ export const loginUser = async (req: Request, res: Response): Promise<void> => {
   const result = await login(username, password);
 
   if (result.status === 200) {
-    req.session.user = result.data.user.username;
+    req.session.user = result.data.user?.username;
   }
 
   res.status(result.status).json(result.data);
